066c9ebd008b92d1f57e4279991ae79336617c2c
EC2 Default User <104750358+morunc4@users.noreply.github.com>
2022-06-16 04:38:03 +0000

    initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,34 @@
+class BooksController < ApplicationController
+
+
+  def index
+    @book=Book.new
+    @books=Book.all
+
+
+  end
+
+  def create
+    @book=Book.new(book_params)
+    @book.user_id= current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def show
+    @book_new=Book.new
+    @book=Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+
+end
